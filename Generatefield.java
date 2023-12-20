ffbe4c5a2b8f06df70e56dec6b51ed8104552401
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2023-12-20 11:05:38 +0100

    7

--- Generatefield.java
+++ Generatefield.java
@@ -1,14 +1,27 @@
 public class Generatefield {
     public static void main(String[] args) {
 
-        // String mit Zahlen die genutz werden
-        String[][] board = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "" }, };
+        int max = 8;
+        int min = 1;
+        int range = max - min + 1;
+
+        String[][] board = { { "1", "4", "7" }, { "2", "6", "5" }, { "3", "8", "" }, };
+
+        // for (int i = 0; i < 8; i++) {
+        // int rand = (int) (Math.random() * range) + min;
+        // String rands = String.valueOf(rand);
+        // for (int j = 0; j < board.length; j++) {
+
+        // }
+        // }
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
+
                 System.out.print(board[i][j] + "\t");
             }
+
+            System.out.println("\n");
         }
-        System.out.println("\n");
     }
 }
