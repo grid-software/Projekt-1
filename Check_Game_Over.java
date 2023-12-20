a4b70baf732b1a892c712465d84bc11770313846
Philipp Ivanov <philipp.ivanov02@gmail.com>
2023-12-20 11:38:47 +0100

    hih

--- Check_Game_Over.java
+++ Check_Game_Over.java
@@ -1,3 +1,10 @@
+
+import java.util.Arrays;
+
 public class Check_Game_Over {
-    
+    public static boolean checkGame(String[][] board) {
+        String[][] endzustand = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "" } };
+
+        return Arrays.deepEquals(board, endzustand);
+    }
 }
