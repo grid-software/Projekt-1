56760bcca9b905286010563ef37974be90fd30dd
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2023-12-20 10:19:00 +0100

    5

--- Generatefield.java
+++ Generatefield.java
@@ -1,11 +1,8 @@
-import java.util.Arrays;
-
 public class Generatefield {
     public static void main(String[] args) {
 
         String[][] board = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "" }, };
 
-        // int[][] board = new int[3][3];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 String s = String.valueOf(i);
@@ -18,17 +15,5 @@ public class Generatefield {
             }
             System.out.println("\n");
         }
-
-        // System.out.println("another way to print 2D arrays");
-        // System.out.println(Arrays.deepToString(board));
-
-        // String[][] verschiebeSpielnummber = { randomStrings, randomStrings,
-        // randomStrings, randomStrings, randomStrings,
-        // randomStrings, randomStrings, randomStrings, randomStrings };
-        // print(verschiebeSpiel);
-    }
-
-    private static void print(String[][] verschiebeSpielnummber) {
-        print(verschiebeSpielnummber);
     }
 }
