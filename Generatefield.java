e4ff38e61281c8d55aaae6115fcbfccc552d3ba3
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2023-12-20 10:08:14 +0100

    4

--- Generatefield.java
+++ Generatefield.java
@@ -1,11 +1,35 @@
+import java.util.Arrays;
+
 public class Generatefield {
     public static void main(String[] args) {
-        String[] randomStrings = {"1","2","3","4","5","6","7","8","",};
-        String[][] verschiebeSpiel = {randomStrings,randomStrings,randomStrings,randomStrings,randomStrings,randomStrings,randomStrings,randomStrings,randomStrings};
-        print(verschiebeSpiel);
+
+        String[][] board = { { "1" }, { "2" }, { "3" }, { "4" }, { "5" }, { "6" }, { "7" }, { "8" }, { "" }, };
+
+
+        // int[][] board = new int[3][3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                String s = String.valueOf(i);
+                board[i][j] = s + j;
+            }
+        }
+
+        for (int[] a : board) {
+            for (int i : a) {
+                System.out.print(i + "\t");
+            }
+            System.out.println("\n");
+        }
+
+        // System.out.println("another way to print 2D arrays");
+        // System.out.println(Arrays.deepToString(board));
+
+        // String[][] verschiebeSpielnummber = { randomStrings, randomStrings, randomStrings, randomStrings, randomStrings,
+                // randomStrings, randomStrings, randomStrings, randomStrings };
+        // print(verschiebeSpiel);
     }
 
-    private static void print(String[][] verschiebeSpiel) {
-        print(verschiebeSpiel);
+    private static void print(String[][] verschiebeSpielnummber) {
+        print(verschiebeSpielnummber);
     }
 }
