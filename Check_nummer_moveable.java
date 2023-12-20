a2eb6f4ac5c0d9d32ba2b7a240a86118258570bc
Stemmi20 <124707316+Stemmi20@users.noreply.github.com>
2023-12-20 09:06:12 +0100

    2

--- Check_nummer_moveable.java
+++ Check_nummer_moveable.java
@@ -1,3 +1,5 @@
 public class Check_nummer_moveable {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- Display.java
+++ Display.java
@@ -1,3 +1,5 @@
 public class Display {
-    
+    public static void main(String[] args) {
+
+    }
 }
